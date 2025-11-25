bc94996f51426b6008e4508f25fbbb1c7722f6b3
Hrithik Biswas <mr.hrithikbiswas@gmail.com>
2025-11-26 03:10:50 +0600

    refactor(prisma): update PrismaClient initialization and remove unused adapter code

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -1,8 +1,24 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
-export const prisma = new PrismaClient({
-    adapter,
-});
+// export const prisma = new PrismaClient({
+//     adapter,
+// });
+
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis;
+
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        log: ['error', 'warn'],
+    });
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
