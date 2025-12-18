8e9981f6a646a4dc24217001b7ee4104ea96dc93
Hrithik Biswas <mr.hrithikbiswas@gmail.com>
2025-12-18 20:35:48 +0600

    Implemented active link functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,18 @@
+import { Jost } from 'next/font/google';
 import { ThemeProvider } from 'next-themes';
 import AuthProvider from '@/context/AuthContext';
 import HeroProvider from '@/providers/HeroProvider';
 import './globals.css';
 import ProductProvider from '@/context/ProductContext';
 
+const montserrat = Jost({ subsets: ['latin'] });
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en" suppressHydrationWarning>
             <body
                 suppressHydrationWarning
-                className={`antialiased min-h-screen dark:bg-gray-950! dark:text-white`}
+                className={`${montserrat.className} antialiased min-h-screen dark:bg-gray-950! dark:text-white`}
             >
                 <ThemeProvider
                     attribute="data-theme"
--- src/components/Header/NavLinks.jsx
+++ src/components/Header/NavLinks.jsx
@@ -1,20 +1,28 @@
+'use client';
 import { NavbarItem } from '@heroui/react';
 import Link from 'next/link';
-import React from 'react';
+import { usePathname } from 'next/navigation';
 
-const NavLinks = ({ links }) => (
-    <>
-        {links.map(({ name, path }, index) => (
-            <NavbarItem key={index}>
-                <Link
-                    href={path}
-                    className="text-xl text-foreground dark:text-background"
-                >
-                    {name}
-                </Link>
-            </NavbarItem>
-        ))}
-    </>
-);
+const NavLinks = ({ links }) => {
+    const pathname = usePathname().split('/')[1];
+    return (
+        <>
+            {links.map(({ name, path }, index) => (
+                <NavbarItem key={index}>
+                    <Link
+                        href={path}
+                        className={`${
+                            path.split('/')[1] === pathname
+                                ? 'after:w-full'
+                                : 'after:w-0'
+                        }  relative text-xl text-foreground dark:text-background after:absolute after:bottom-0 after:left-0 after:h-[1px] after:bg-foreground after:transition-all after:duration-200 hover:after:w-full`}
+                    >
+                        {name}
+                    </Link>
+                </NavbarItem>
+            ))}
+        </>
+    );
+};
 
 export default NavLinks;
